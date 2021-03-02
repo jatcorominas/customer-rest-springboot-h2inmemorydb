cf509e5cae0b67c1767673fccbea950b7b5a2d5e
jatcorominas <jatcorominas@gmail.com>
2021-03-02 10:04:10 -0600

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,10 @@
-insert into customer values(10001,FALSE,23,'Jack');
-insert into customer values(10002,FALSE,23,'Kathryn');
-insert into customer values(10003,FALSE,27,'Adam');
+DROP TABLE IF EXISTS customer;
+create table customer (
+	ID INT PRIMARY KEY,
+	NAME VARCHAR(30) NOT NULL,
+	AGE INT NOT NULL,
+	ACTIVE BOOLEAN
+);
+insert into customer (ID,ACTIVE,AGE,NAME) values(10001,FALSE,23,'Jack');
+insert into customer (ID,ACTIVE,AGE,NAME) values(10002,FALSE,23,'Kathryn');
+insert into customer (ID,ACTIVE,AGE,NAME) values(10003,FALSE,27,'Adam');
